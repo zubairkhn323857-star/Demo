9e8c154be4f44d86bc288dea96f38f876628be35
Abu Zubair <zubair123@gmail.com>
2026-07-01 21:10:56 +0530

    first commit

---
+++ datatype.java
@@ -0,0 +1,21 @@
+class datatype
+{
+public static void main(String[] args){
+int a=10;
+byte b=12;
+short c=13;
+long d=1234467890;
+char e='A';
+boolean f=true;
+float g=12.3f;
+double h=123.4567;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+}
+}
